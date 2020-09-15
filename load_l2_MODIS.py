1dd4cec021234044aedb070198ecb83c8066001b
Alyson Douglas <rosealyd@sci1.jasmin.ac.uk>
2020-09-15 21:54:06 +0100

    fixed ctt

--- load_l2_MODIS.py
+++ load_l2_MODIS.py
@@ -12,24 +12,26 @@ test_filename = '/gws/nopw/j04/eo_shared_data_vol1/satellite/modis/modis_c61/myd
 level_data = SD(test_filename, SDC.READ)
 re = level_data.select('Cloud_Effective_Radius').get()
 lwp = level_data.select('Cloud_Water_Path').get()/1000.
-cth = level_data.select('Cloud_Top_Height').get()
+cth = level_data.select('Cloud_Top_Height').get()/1000.
 
 ctt = level_data.select('Cloud_Top_Temperature').get() - level_data.select('Cloud_Top_Temperature').attributes()['add_offset'] 
 ctt = ctt * level_data.select('Cloud_Top_Temperature').attributes()['scale_factor']
-print(np.amin(ctt), np.amax(ctt))
+
 latitutdes = level_data.select('Latitude').get()
 longitudes = level_data.select('Longitude').get()
 
-plt.subplot(121)
-plt.pcolormesh(lwp)
-plt.colorbar()
-print(ctt)
-print (np.amin(ctt), np.amax(ctt))
-plt.subplot(122)
-plt.pcolormesh(ctt, vmin = 150, vmax = 350)
+cold = (ctt < 253)
+ctt[cold] = np.nan
+
+N_wc_pixels = np.count_nonzero(~np.isnan(ctt))
+N_pixels = float(ctt.shape[0] * ctt.shape[1])
+
+print(N_wc_pixels/N_pixels)
+
+plt.pcolormesh(ctt, vmin = 253)
 plt.colorbar()
 plt.show()
-sys.exit()
+
 
 
 
