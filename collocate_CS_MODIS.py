0dfa528efdff4b62f132646950d1a00e9b1a98d7
Alyson Douglas <rosealyd@host595.jc.rl.ac.uk>
2020-11-01 14:57:31 +0000

    updated for jasmin interface

--- collocate_CS_MODIS.py
+++ collocate_CS_MODIS.py
@@ -2,59 +2,62 @@
 import sys
 import numpy as np
 import copy
+import glob
+from datetime import timedelta
 import datetime
+from pyhdf.SD import SD, SDC
 from sklearn.neighbors import KDTree
 import matplotlib.pyplot as plt
-import src
 
 
+#note this does not take care of the problem of needing leading zeroes on cloudsat diys
 	
-diy = 57
+day = '12'
+month = '05'
 year = 2008
 
-print 'getting MODIS grids for ', diy, year
+#note, americans would prefer month day year but for the global good i did day month year
+print('getting MODIS grids for ', day, month, year)
+modis_dir = '/neodc/modis/data/MOD04_L2/collection61/{}/{}/{}/'
+modis_files = sorted(glob.glob(modis_dir.format(year, month, day) + '*.hdf'))
+print(modis_files)
+print('getting cloudsat lats/lons')
+diy = datetime.date(year = year, month = int(month), day = int(day)) - datetime.date(year = year, month = 1, day = 1)
+diy = diy.days
+day_before = datetime.date(year = year, month = int(month), day = int(day)) - datetime.timedelta(days = 1)
+day_before = (day_before - datetime.date(year = year, month = 1, day = 1)).days
+print(diy, day_before)
+cloudsat_dir = '/gws/nopw/j04/eo_shared_data_vol1/satellite/cloudsat/2b-geoprof/R05/{}/{}/'
+cloudsat_files = sorted(glob.glob(cloudsat_dir.format(year, day_before) + '*.hdf'))
+cloudsat_day_of = sorted(glob.glob(cloudsat_dir.format(year, diy) + '*.hdf'))
+cloudsat_files.extend(cloudsat_day_of)
+#note: cloudsat filenames go year, day, HH MM SS at the start
 
-modis_dict = src.get_modis_lls(diy, year)
+#chunk cloudsat by 5 minutes
+cloudsat_chunks = {}
+for cloudsat_granule in cloudsat_files:
+	print(cloudsat_granule)
+	cloudsat_data = SD(cloudsat_granule, SDC.READ)
+	for var in cloudsat_data.datasets():
+		print(var)
+	sys.exit()
 
-print 'getting cloudsat lats/lons'
-X = src.get_cs(diy, year)
 
-print 'building tree'
-tree = KDTree(X)
-
-for modis_filename, grid_dict in modis_dict.iteritems():
-	print 'doing', modis_filename
-	modis_lats = grid_dict['lats']
-	modis_lons = grid_dict['lons']
-	indices_modis = []
-	distances = []
-	indices_cloudsat = []
+print('getting MODIS files as X')
+for filename in modis_files:
+	print(filename)
+	modis_locs_data = SD(filename, SDC.READ)
+	modis_latitudes = modis_locs_data.select('Latitude')
+	modis_longitudes = modis_locs_data.select('Longitude')
 	
-	lats_cloudsat = []
-	lats_modis = []
-	
-	print 'finding indices'
-	for n in range(len(modis_lats)):
-		#contains the distance, index of X, modis lat lon, modis indices, and cloudsat lat lons
-		dis_idx_mll_midx_csll = []
-		for i, ll in enumerate(zip(modis_lats[n], modis_lons[n])):
-			
-			distance, index = tree.query([ll])
-			csll = X[index[0][0]]
-			
-			dis_idx_mll_midx_csll.append((distance[0][0], index[0][0], ll, (n, i), csll))
-		min_d = min(dis_idx_mll_midx_csll)
-		print min_d[2], min_d[-1]
-		indices_modis.append(min_d[3])
-		indices_cloudsat.append(index[0][0])
-		distances.append(min_d[0])
-	
-		lats_cloudsat.append(min_d[-1][0])
-		lats_modis.append(min_d[2][0])
-	plt.plot(distances)
-	plt.show()
-	plt.plot(lats_modis, lats_cloudsat)
-	plt.show()
+	sys.exit()
+
+
+
+
+
+
+
 
 
 
